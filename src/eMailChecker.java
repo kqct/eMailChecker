b57db52cbb4753fb29b98bba1f862d9507490611
meow10811 <meow10811+github@gmail.com>
2017-05-14 19:39:44 -0400

    Created UI

--- src/eMailChecker.java
+++ src/eMailChecker.java
@@ -1,6 +1,61 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
 /**
  * Created by Josh on 5/12/17.
  */
 
-public class eMailChecker {
+public class eMailChecker extends JFrame implements ActionListener{
+    String emailToValidate;
+    String regex = "[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z0-9]+";
+
+    Color valid = new Color(1,83,42);
+    Color invalid = new Color(147,0,15);
+
+    JTextField textField = new JTextField(20);
+    JButton validate = new JButton("Validate");
+    JLabel isValid = new JLabel(" ");
+
+    public eMailChecker() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(),BoxLayout.PAGE_AXIS));
+
+        textField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(textField);
+
+        validate.setAlignmentX(Component.CENTER_ALIGNMENT);
+        validate.addActionListener(this);
+        add(validate);
+
+        isValid.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(isValid);
+
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == validate) {
+            emailToValidate = textField.getText();
+            if (validateEmail(emailToValidate)){
+                isValid.setText(emailToValidate + " is valid!");
+                isValid.setForeground(valid);
+            } else {
+                isValid.setText(emailToValidate + " is invalid!");
+                isValid.setForeground(invalid);
+            }
+
+        }
+    }
+
+    public Boolean validateEmail(String email) {
+        return Boolean.TRUE;
+    }
+
+    public static void main(String[] args) {
+        eMailChecker e = new eMailChecker();
+
+    }
 }
